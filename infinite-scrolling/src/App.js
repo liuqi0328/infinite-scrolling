51a75828a7cb5a9dfbc25062a2365510ebbb2ae7
liuqi0328 <lqyterrible@gmail.com>
2019-10-01 21:30:52 -0700

    Initialized with create-react-app

---
+++ infinite-scrolling/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function App() {
+  return (
+    <>
+      <input type="text"></input>
+      <div>Title</div>
+      <div>Title</div>
+      <div>Title</div>
+      <div>Title</div>
+      <div>Loading...</div>
+      <div>Error</div>
+    </>
+  )
+}
+
+export default App;
